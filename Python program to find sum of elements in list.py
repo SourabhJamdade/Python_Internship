639edda4a1cbea86a6d7b0bdcca2bbee65c2eb42
Sourabh Jamdade <94708798+SourabhJamdade@users.noreply.github.com>
2021-12-06 20:38:08 +0530

    Create Python program to find sum of elements in list.py

---
+++ Python program to find sum of elements in list.py
@@ -0,0 +1,11 @@
+#Python program to find sum of elements in list
+a=[22,97,67,24,77,56,12,87,35]
+sum=0
+print("\n Given List : \n ",a)
+for x in a:
+	sum+=x
+print("\n Sum Of All Elements Is : ",sum)
+
+#for x in range(len(a)):
+#	sum+=a[x]
+#print("\n Sum Of All Elements Is : ",sum)
